c946e2b2224d94a68bcd3dd3cefebc4774713ff1
Tinat1ni <tinatingagnidze121@gmail.com>
2024-09-12 14:37:17 +0400

    changing the code to save the information in a file as soon as we get it

--- my_thread.py
+++ my_thread.py
@@ -6,25 +6,30 @@ from concurrent.futures import ThreadPoolExecutor
 
 green_light_red_light = threading.Lock()
 
-results = []
-
-def get_post(post_id):
+def get_post(post_id, file):
     url = f'https://jsonplaceholder.typicode.com/posts/{post_id}'
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         with green_light_red_light:
-            results.append(data)
+            with open(file, 'a') as f:
+                if f.tell() == 0:
+                    f.write('[')
+                else:
+                    f.write(', ')
+                json.dump(data, f, indent = 4)
 
 
 def main():
-    with ThreadPoolExecutor(max_workers = 11) as executor:
-        executor.map(get_post, range(1,78))
+    file = 'data.json'
+    with open(file, 'w') as f:
+        f.write('')
 
-    with green_light_red_light:
-        with open('data.json', 'w') as f:
-            json.dump(results, f, indent = 4)
+    with ThreadPoolExecutor(max_workers=11) as executor:
+        executor.map(lambda post_id: get_post(post_id, file), range(1, 78))
 
+    with open(file, 'a') as f:
+        f.write(']')
 
 
 if __name__ == '__main__':
